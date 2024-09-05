4a4d17b72f6ab6e6c165aea78901dce21cd7651e
alexbeserra <alexbeserrab@gmail.com>
2024-09-05 12:27:18 -0300

    Inclusao CSS

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+//Components
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBindingComponent],
   template: ` 
   <!-- <router-outlet />  -->
+  <!-- <app-new-component /> -->
   <h1>Curso de Angular</h1>
-  <app-new-component />
+  <app-template-binding />  
   `
 })
 export class AppComponent {}
